7eb5ff495c72dd76b0b519b762ccd853b824647e
Arabelle <arabelle310@yahoo.ca>
2017-03-26 18:49:00 -0700

    Add a login gui

--- src/ConnectDB.java
+++ src/ConnectDB.java
@@ -1,32 +1,474 @@
+
+// We need to import the java.sql package to use JDBC
 import java.sql.*;
-import java.util.*;
-import java.text.*;
-	
-public class ConnectDB {
-	private static String username = "ora_xxxx@ug";
-	private static String pwd = "xxxxxxxxx";
-	
-	
-	
-	public static void main(String args[]) throws SQLException, ParseException {
-		DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
-		Connection con = DriverManager.getConnection("jdbc:oracle:thin:@dbhost.ugrad.cs.ubc.ca:1522:ug", username, pwd);
-		
-		con.setAutoCommit(false);
-		PreparedStatement ps = con.prepareStatement("INSERT INTO equipment VALUES (?, ?, ?)");
-		
-		ps.setInt(1, 200);
-		ps.setString(2, "gear");
-		ps.setString(3, "hockey stick");
-		ps.executeUpdate();
-		con.commit();
-		Statement stmt = con.createStatement();
-		ResultSet rs = stmt.executeQuery("SELECT * FROM equipment");
-		while(rs.next())
-		{
-			System.out.println("ID: " + Integer.toString(rs.getInt(1)));
-			System.out.println("Type: " + rs.getString("item_type"));
-			System.out.println("Name: " + rs.getString("item_name"));
-		}
+
+// for reading from the command line
+import java.io.*;
+
+// for the login window
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
+/*
+ * This class implements a graphical login window and a simple text
+ * interface for interacting with the branch table 
+ */ 
+public class ConnectDB implements ActionListener
+{
+    // command line reader 
+    private BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+    private Connection con;
+
+    // user is allowed 3 login attempts
+    private int loginAttempts = 0;
+
+    // components of the login window
+    private JTextField usernameField;
+    private JPasswordField passwordField;
+    private JFrame mainFrame;
+
+
+    /*
+     * constructs login window and loads JDBC driver
+     */ 
+    public ConnectDB()
+    {
+      mainFrame = new JFrame("User Login");
+
+      JLabel usernameLabel = new JLabel("Enter username: ");
+      JLabel passwordLabel = new JLabel("Enter password: ");
+
+      usernameField = new JTextField(10);
+      passwordField = new JPasswordField(10);
+      passwordField.setEchoChar('*');
+
+      JButton loginButton = new JButton("Log In");
+
+      JPanel contentPane = new JPanel();
+      mainFrame.setContentPane(contentPane);
+
+
+      // layout components using the GridBag layout manager
+
+      GridBagLayout gb = new GridBagLayout();
+      GridBagConstraints c = new GridBagConstraints();
+
+      contentPane.setLayout(gb);
+      contentPane.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+      // place the username label 
+      c.gridwidth = GridBagConstraints.RELATIVE;
+      c.insets = new Insets(10, 10, 5, 0);
+      gb.setConstraints(usernameLabel, c);
+      contentPane.add(usernameLabel);
+
+      // place the text field for the username 
+      c.gridwidth = GridBagConstraints.REMAINDER;
+      c.insets = new Insets(10, 0, 5, 10);
+      gb.setConstraints(usernameField, c);
+      contentPane.add(usernameField);
+
+      // place password label
+      c.gridwidth = GridBagConstraints.RELATIVE;
+      c.insets = new Insets(0, 10, 10, 0);
+      gb.setConstraints(passwordLabel, c);
+      contentPane.add(passwordLabel);
+
+      // place the password field 
+      c.gridwidth = GridBagConstraints.REMAINDER;
+      c.insets = new Insets(0, 0, 10, 10);
+      gb.setConstraints(passwordField, c);
+      contentPane.add(passwordField);
+
+      // place the login button
+      c.gridwidth = GridBagConstraints.REMAINDER;
+      c.insets = new Insets(5, 10, 10, 10);
+      c.anchor = GridBagConstraints.CENTER;
+      gb.setConstraints(loginButton, c);
+      contentPane.add(loginButton);
+
+      // register password field and OK button with action event handler
+      passwordField.addActionListener(this);
+      loginButton.addActionListener(this);
+
+      // anonymous inner class for closing the window
+      mainFrame.addWindowListener(new WindowAdapter() 
+      {
+	public void windowClosing(WindowEvent e) 
+	{ 
+	  System.exit(0); 
 	}
+      });
+
+      // size the window to obtain a best fit for the components
+      mainFrame.pack();
+
+      // center the frame
+      Dimension d = mainFrame.getToolkit().getScreenSize();
+      Rectangle r = mainFrame.getBounds();
+      mainFrame.setLocation( (d.width - r.width)/2, (d.height - r.height)/2 );
+
+      // make the window visible
+      mainFrame.setVisible(true);
+
+      // place the cursor in the text field for the username
+      usernameField.requestFocus();
+
+      try 
+      {
+	// Load the Oracle JDBC driver
+	DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+      }
+      catch (SQLException ex)
+      {
+	System.out.println("Message: " + ex.getMessage());
+	System.exit(-1);
+      }
+    }
+
+
+    /*
+     * connects to Oracle database named ug using user supplied username and password
+     */ 
+    private boolean connect(String username, String password)
+    {
+      String connectURL = "jdbc:oracle:thin:@localhost.ugrad.cs.ubc.ca:1522:ug"; 
+
+      try 
+      {
+	con = DriverManager.getConnection(connectURL,username,password);
+
+	System.out.println("\nConnected to Oracle!");
+	return true;
+      }
+      catch (SQLException ex)
+      {
+	System.out.println("Message: " + ex.getMessage());
+	return false;
+      }
+    }
+
+
+    /*
+     * event handler for login window
+     */ 
+    public void actionPerformed(ActionEvent e) 
+    {
+	if ( connect(usernameField.getText(), String.valueOf(passwordField.getPassword())) )
+	{
+	  // if the username and password are valid, 
+	  // remove the login window and display a text menu 
+	  mainFrame.dispose();
+          showMenu();     
+	}
+	else
+	{
+	  loginAttempts++;
+	  
+	  if (loginAttempts >= 3)
+	  {
+	      mainFrame.dispose();
+	      System.exit(-1);
+	  }
+	  else
+	  {
+	      // clear the password
+	      passwordField.setText("");
+	  }
+	}             
+                    
+    }
+
+
+    /*
+     * displays simple text interface
+     */ 
+    private void showMenu()
+    {
+	int choice;
+	boolean quit;
+
+	quit = false;
+	
+	try 
+	{
+	    // disable auto commit mode
+	    con.setAutoCommit(false);
+
+	    while (!quit)
+	    {
+		System.out.print("\n\nPlease choose one of the following: \n");
+		System.out.print("1.  Insert Equipment\n");
+		System.out.print("2.  Delete Equipment\n");
+		System.out.print("3.  Update Equipment\n");
+		System.out.print("4.  Show Equipment\n");
+		System.out.print("5.  Quit\n>> ");
+
+		choice = Integer.parseInt(in.readLine());
+		
+		System.out.println(" ");
+
+		switch(choice)
+		{
+		   case 1:  insertEquipment(); break;
+		   case 2:  deleteEquipment(); break;
+		   case 3:  updateNameEquipment(); break;
+		   case 4:  showEquipment(); break;
+		   case 5:  quit = true;
+		}
+	    }
+
+	    con.close();
+            in.close();
+	    System.out.println("\nGood Bye!\n\n");
+	    System.exit(0);
+	}
+	catch (IOException e)
+	{
+	    System.out.println("IOException!");
+
+	    try
+	    {
+		con.close();
+		System.exit(-1);
+	    }
+	    catch (SQLException ex)
+	    {
+		 System.out.println("Message: " + ex.getMessage());
+	    }
+	}
+	catch (SQLException ex)
+	{
+	    System.out.println("Message: " + ex.getMessage());
+	}
+    }
+
+
+    /*
+     * inserts an equipment
+     */ 
+    private void insertEquipment()
+    {
+	int                iid;
+	String             iname;
+	String             itype;
+	PreparedStatement  ps;
+	  
+	try
+	{
+	  ps = con.prepareStatement("INSERT INTO equipment VALUES (?,?,?)");
+	
+	  System.out.println("\nEquipment ID: ");
+	  iid = Integer.parseInt(in.readLine());
+	  ps.setInt(1, iid);
+
+	  System.out.println("\nEquipment Type: ");
+	  itype = in.readLine();
+	  ps.setString(2, itype);
+	  
+	  System.out.println("\nEquipment Name: ");
+	  iname = in.readLine();
+	  ps.setString(3, iname);
+
+	  ps.executeUpdate();
+
+	  // commit work 
+	  con.commit();
+
+	  ps.close();
+	}
+	catch (IOException e)
+	{
+	    System.out.println("IOException!");
+	}
+	catch (SQLException ex)
+	{
+	    System.out.println("Message: " + ex.getMessage());
+	    try 
+	    {
+		// undo the insert
+		con.rollback();	
+	    }
+	    catch (SQLException ex2)
+	    {
+		System.out.println("Message: " + ex2.getMessage());
+		System.exit(-1);
+	    }
+	}
+    }
+
+
+    /*
+     * deletes an equipment
+     */ 
+    private void deleteEquipment()
+    {
+	int                iid;
+	PreparedStatement  ps;
+	  
+	try
+	{
+	  ps = con.prepareStatement("DELETE FROM equipment WHERE item_id = ?");
+	
+	  System.out.print("\nEquipment ID: ");
+	  iid = Integer.parseInt(in.readLine());
+	  ps.setInt(1, iid);
+
+	  int rowCount = ps.executeUpdate();
+
+	  if (rowCount == 0)
+	  {
+	      System.out.println("\nEquipment " + iid + " does not exist!");
+	  }
+
+	  con.commit();
+
+	  ps.close();
+	}
+	catch (IOException e)
+	{
+	    System.out.println("IOException!");
+	}
+	catch (SQLException ex)
+	{
+	    System.out.println("Message: " + ex.getMessage());
+
+            try 
+	    {
+		con.rollback();	
+	    }
+	    catch (SQLException ex2)
+	    {
+		System.out.println("Message: " + ex2.getMessage());
+		System.exit(-1);
+	    }
+	}
+    }
+    
+
+    /*
+     * updates the name of an equipment
+     */ 
+    private void updateNameEquipment()
+    {
+	int                iid;
+	String             iname;
+	PreparedStatement  ps;
+	  
+	try
+	{
+	  ps = con.prepareStatement("UPDATE branch SET item_name = ? WHERE item_id = ?");
+	
+	  System.out.print("\nEquipment ID: ");
+	  iid = Integer.parseInt(in.readLine());
+	  ps.setInt(2, iid);
+
+	  System.out.print("\nEquipment Name: ");
+	  iname = in.readLine();
+	  ps.setString(1, iname);
+
+	  int rowCount = ps.executeUpdate();
+	  if (rowCount == 0)
+	  {
+	      System.out.println("\nEquipment" + iid + " does not exist!");
+	  }
+
+	  con.commit();
+
+	  ps.close();
+	}
+	catch (IOException e)
+	{
+	    System.out.println("IOException!");
+	}
+	catch (SQLException ex)
+	{
+	    System.out.println("Message: " + ex.getMessage());
+	    
+	    try 
+	    {
+		con.rollback();	
+	    }
+	    catch (SQLException ex2)
+	    {
+		System.out.println("Message: " + ex2.getMessage());
+		System.exit(-1);
+	    }
+	}	
+    }
+
+    
+    /*
+     * display information about branches
+     */ 
+    private void showEquipment()
+    {
+	String     iid;
+	String     itype;
+	String     iname;
+	Statement  stmt;
+	ResultSet  rs;
+	   
+	try
+	{
+	  stmt = con.createStatement();
+
+	  rs = stmt.executeQuery("SELECT * FROM equipment");
+
+	  // get info on ResultSet
+	  ResultSetMetaData rsmd = rs.getMetaData();
+
+	  // get number of columns
+	  int numCols = rsmd.getColumnCount();
+
+	  System.out.println(" ");
+	  
+	  // display column names;
+	  for (int i = 0; i < numCols; i++)
+	  {
+	      // get column name and print it
+
+	      System.out.printf("%-15s", rsmd.getColumnName(i+1));    
+	  }
+
+	  System.out.println(" ");
+
+	  while(rs.next())
+	  {
+	      // for display purposes get everything from Oracle 
+	      // as a string
+
+	      // simplified output formatting; truncation may occur
+
+	      iid = rs.getString("item_id");
+	      System.out.printf("%-10.10s", iid);
+
+	      itype = rs.getString("item_type");
+	      System.out.printf("%-15.15s", itype);
+	      
+	      iname = rs.getString("item_name");
+	      System.out.printf("%-20.20s", iname);
+	      
+	      System.out.print("\n");
+
+	  }
+ 
+	  // close the statement; 
+	  // the ResultSet will also be closed
+	  stmt.close();
+	}
+	catch (SQLException ex)
+	{
+	    System.out.println("Message: " + ex.getMessage());
+	}	
+    }
+    
+ 
+    public static void main(String args[])
+    {
+      ConnectDB condb = new ConnectDB();
+    }
 }
